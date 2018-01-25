17a73296117ff9463bd5c26a35ddead03182fd9c
Okuyama Takafumi <okuyama1625@gmail.com>
2018-01-26 02:10:58 +0900

    modelを打ち込めば結果が返ってくる

--- python-agentRobo2/plot_reward_log_test.py
+++ python-agentRobo2/plot_reward_log_test.py
@@ -9,62 +9,77 @@ def get_better_model(model_name):
     s = df.columns[1]
     g = df.columns[3]
 
-    score = np.array(df[s])[:1000].astype(float)
-    goal_time = np.array(df[g])[:1000]
+    score = np.array(df[s])[:num_model*num_test].astype(float)
+    goal_time = np.array(df[g])[:num_model*num_test]
 
-    num_model = score.shape[0]/10
-    print "Number of Model : ",num_model
-
-    score = score.reshape((num_model,10))
-    goal_time = goal_time.reshape((num_model,10))
+    score = score.reshape((num_model,num_test))
+    goal_time = goal_time.reshape((num_model,num_test))
 
     clear_count_list = []
     better_model_index = []
+    '''
     with open("./betterModelLog_fix.csv", 'a') as the_file:
-        the_file.write("%s,,\nModel,AverageScore,MaxScore,MinScore,AverageTime,FastestTime,SlowestTime\n"%(model_name))
-    for i in range(score.shape[0]):
+        the_file.write("%s,,\nModel,Clear Count,AverageScore,MaxScore,MinScore,AverageTime,FastestTime,SlowestTime\n"%(model_name))
+    '''
+    for i in range(num_model):
         clear_count = np.sum(goal_time[i] > 0)
         clear_count_list.append(clear_count)
-
-        if(clear_count>=9):
+        '''
+        if(clear_count==10):
             better_model_index.append(i)
             with open("./betterModelLog_fix.csv", 'a') as the_file:
                 the_file.write(str((i+1)*10000) +
+                           ',' + str(clear_count) +
                            ',' + str(np.average(score[i])) +
                            ',' + str(np.max(score[i])) +
                            ',' + str(np.min(score[i])) +
                            ',' + str(np.average(goal_time[i])) +
                            ',' + str(np.min(goal_time[i][goal_time[i]>0])) +
                            ',' + str(np.max(goal_time[i])) + '\n')
+        '''
 
     return clear_count_list
 
-with open("./betterModelLog_fix.csv", 'w') as the_file:
-    the_file.write("Better Models\n")
+#with open("./betterModelLog_fix.csv", 'w') as the_file:
+    #the_file.write("Better Models\n")
 
-
+num_model = 30
+num_test = 10
 #model_name_list = ["Action3","Action5","Action7_1","Action7_2"]
 #model_name_list = ["Action3V","Action5V","Action7_1V","Action7_2V"]
-model_name_list = ["Action3_fix"]
-dis_x = 100 * 10**4
-plt.xticks(range(0,dis_x +1,dis_x/5))
+model_name_list = ["Action3_fix","Action3V_fix"]
+#model_name_list = ["Action3A","Action3VA"]
+
+print "Number of Model : ",num_model
+print "Number of Test : ",num_test
+
+x = range(10000,num_model*10**4+1,10000)
+
+plt.xticks(range(0,num_model*10**4+1,num_model*10**4/5))
 plt.xlabel("Cycle") # x軸のラベル
 plt.ylabel("Number of goal") # y軸のラベル
-x = range(10000,1000001,10000)
+
 for model_name in model_name_list:
     ccl = get_better_model(model_name)
-    '''
-    if(model_name == model_name_list[0]):
-        plt.plot(x[:dis_x/10000], ccl[:dis_x/10000], label='Model1', color='red', linewidth=2.5)
-    elif(model_name == model_name_list[1]):
-        plt.plot(x[:dis_x/10000], ccl[:dis_x/10000], label='Model2', color='green', linewidth=2.5)
-    elif(model_name == model_name_list[2]):
-        plt.plot(x[:dis_x/10000], ccl[:dis_x/10000], label='Model3', color='blue', linewidth=2.5)
-    elif(model_name == model_name_list[3]):
-        plt.plot(x[:dis_x/10000], ccl[:dis_x/10000], label='Model4', color='#ffc700', linewidth=2.5)
-    plt.legend(loc = 'upper right') #これをしないと凡例出てこない(lower⇆upper, left⇆ center ⇆right)
+
+    if(len(model_name_list)==4):
+        if(model_name == model_name_list[0]):
+            plt.plot(x[:num_model], ccl[:num_model], label='Model1V', color='red', linewidth=2.5)
+        elif(model_name == model_name_list[1]):
+            plt.plot(x[:num_model], ccl[:num_model], label='Model2V', color='green', linewidth=2.5)
+        elif(model_name == model_name_list[2]):
+            plt.plot(x[:num_model], ccl[:num_model], label='Model3V', color='blue', linewidth=2.5)
+        elif(model_name == model_name_list[3]):
+            plt.plot(x[:num_model], ccl[:num_model], label='Model4V', color='#ffc700', linewidth=2.5)
+
+    elif(len(model_name_list)==2):
+        if(model_name == model_name_list[0]):
+            plt.plot(x[:num_model], ccl[:num_model], label='Model1_forward', color='red', linewidth=2.5)
+        elif(model_name == model_name_list[1]):
+            plt.plot(x[:num_model], ccl[:num_model], label='Model1V_forward', color='green', linewidth=2.5)
+
+    plt.legend(loc = 'upper left') #これをしないと凡例出てこない(lower⇆upper, left⇆ center ⇆right)
     plt.show()
-    '''
 
 
 '''
--- python-agentRobo2/plot_reward_log_train.py
+++ python-agentRobo2/plot_reward_log_train.py
@@ -5,47 +5,86 @@ import pandas as pd
 import argparse
 import numpy as np
 
-
-df1 = pd.read_csv('cut_rewardAction3V.log')
-df2 = pd.read_csv('cut_rewardAction5V.log')
-df3 = pd.read_csv('cut_rewardAction7_1V.log')
-df4 = pd.read_csv('cut_rewardAction7_2V.log')
+df1 = pd.read_csv('cut_rewardAction3_fix.log')
+df2 = pd.read_csv('cut_rewardAction3V_fix.log')
+#df1 = pd.read_csv('cut_rewardAction3A.log')
+#df2 = pd.read_csv('cut_rewardAction3VA.log')
 
 x1 = df1.columns[0]
 y1 = df1.columns[1]
 x2 = df2.columns[0]
 y2 = df2.columns[1]
-x3 = df3.columns[0]
-y3 = df3.columns[1]
-x4 = df4.columns[0]
-y4 = df4.columns[1]
+#x3 = df3.columns[0]
+#y3 = df3.columns[1]
+#x4 = df4.columns[0]
+#y4 = df4.columns[1]
 
-np.array(df1[y1]).max()
-np.array(df2[y2]).max()
-np.array(df3[y3]).max()
-np.array(df4[y4]).max()
 
-df1[y1] = pd.rolling_mean(df1[y1], window=500)
-df2[y2] = pd.rolling_mean(df2[y2], window=500)
-df3[y3] = pd.rolling_mean(df3[y3], window=500)
-df4[y4] = pd.rolling_mean(df4[y4], window=500)
+df1[y1] = pd.rolling_mean(df1[y1], window=300)
+df2[y2] = pd.rolling_mean(df2[y2], window=300)
+#df3[y3] = pd.rolling_mean(df3[y3], window=500)
+#df4[y4] = pd.rolling_mean(df4[y4], window=500)
+
+y1_array = np.array(df1[y1])
+y2_array = np.array(df2[y2])
+#y3_array = np.array(df3[y3])
+#y4_array = np.array(df4[y4])
+
+print y1_array.max()
+print y2_array.max()
 
 fig, ax = plt.subplots(1, 1)
-#plt.xticks(range(0,15001,2000))
-plt.xticks(range(0,1000001,200000))
+#plt.xticks(range(0,1000001,200000))
+#plt.xticks(range(0,300001,50000))
+plt.xticks(range(0,600001,100000))
+
 plt.xlabel("Cycle") # x軸のラベル
 plt.ylabel("Score") # y軸のラベル
-#plt.xlim(-1, 7) # xを-0.5-7.5の範囲に限定
 
 '''
 plt.plot(list(df1[x1])[:15001], list(df1[y1])[:15001], label='Model1', color='red', linewidth=2.5)
 plt.plot(list(df2[x2])[:15001], list(df2[y2])[:15001], label='Model2', color='green', linewidth=2.5)
 plt.plot(list(df3[x3])[:15001], list(df3[y3])[:15001], label='Model3', color='blue', linewidth=2.5)
 plt.plot(list(df4[x4])[:15001], list(df4[y4])[:15001], label='Model4', color='#ffc700', linewidth=2.5)
+
+plt.plot(list(df1[x1]), y1_array, label='Model1V', color='red', linewidth=2.5)
+plt.plot(list(df2[x2]), y2_array, label='Model1V', color='green', linewidth=2.5)
+plt.plot(list(df3[x3]), y3_array, label='Model3V', color='blue', linewidth=2.5)
+plt.plot(list(df4[x4]), y4_array, label='Model4V', color='#ffc700', linewidth=2.5)
+
+
+plt.plot(list(df1[x1]), y1_array, label='Model1_forward', color='red', linewidth=2.5)
+plt.plot(list(df2[x2]), y2_array, label='Model1V_forward', color='green', linewidth=2.5)
 '''
-plt.plot(list(df1[x1]), list(df1[y1]), label='Model1', color='red', linewidth=2.5)
-plt.plot(list(df2[x2]), list(df2[y2]), label='Model2', color='green', linewidth=2.5)
-plt.plot(list(df3[x3]), list(df3[y3]), label='Model3', color='blue', linewidth=2.5)
-plt.plot(list(df4[x4]), list(df4[y4]), label='Model4', color='#ffc700', linewidth=2.5)
-plt.legend(loc = 'upper right') #これをしないと凡例出てこない(lower⇆upper, left⇆ center ⇆right)
+plt.plot(list(df1[x1]), y1_array, label='Model1A', color='red', linewidth=2.5)
+plt.plot(list(df2[x2]), y2_array, label='Model1VA', color='green', linewidth=2.5)
+plt.legend(loc = 'upper left') #これをしないと凡例出てこない(lower⇆upper, left⇆ center ⇆right)
 plt.show()
+
+'''
+df = pd.read_csv('cut_rewardAction3A.log')
+c = df.columns[0]
+s = df.columns[1]
+e = df.columns[2]
+g = df.columns[3]
+
+cycle = np.array(df[c])[:10000]
+score = np.array(df[s])[:10000].astype(float)
+episode = np.array(df[e])[:10000]
+goal_time = np.array(df[g])[:10000]
+
+np.average(score[501:1000]-2)
+
+score[0:1000] = score[0:1000] -1
+
+score[1000:3000] = score[1000:3000] -1
+
+with open("hoge.log", 'w') as the_file:
+                the_file.write('Cycle,Score,Episode,GoalTime \n')
+for i in range(goal_time.shape[0]):
+    with open("hoge.log", 'a') as the_file:
+        the_file.write(str(cycle[i]) +
+       ',' + str(score[i]) +
+       ',' + str(episode[i]) +
+       ',' + str(goal_time[i]) + '\n')
+'''
